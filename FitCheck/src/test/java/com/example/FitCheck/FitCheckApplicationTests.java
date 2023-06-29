e9d1707dc9267443071d93c4ec1bee0926137dd4
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-06-29 19:12:33 -0400

    Spring Boot

---
+++ FitCheck/src/test/java/com/example/FitCheck/FitCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FitCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
