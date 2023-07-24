49d088e308615ce4d4558efc48c4792638ac74ed
Himel Uddin <hmu4127@rit.edu>
2023-07-24 17:44:05 -0400

    database set up

---
+++ FitCheck_spring/src/main/java/com/FitCheck/FitCheck_Spring/FitCheckSpringApplication.java
@@ -0,0 +1,13 @@
+package com.FitCheck.FitCheck_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCheckSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitCheckSpringApplication.class, args);
+	}
+
+}
---
+++ FitCheck_spring/src/test/java/com/FitCheck/FitCheck_Spring/FitCheckSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FitCheck.FitCheck_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitCheckSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
