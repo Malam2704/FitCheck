6dc075d0445db125c145aecd1efbf60c7202ccb5
Himel Uddin <hmu4127@rit.edu>
2023-07-24 18:18:49 -0400

    Merge Conflict

---
+++ FitCheck_spring/src/main/java/com/FitCheck/FitCheck_Spring/api/FitCheckAPIController.java
@@ -0,0 +1,41 @@
+package com.FitCheck.FitCheck_Spring.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") // Base URL mapping for all endpoints in this controller
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    // POST: initially adding clothing piece to the db (clothing attributes...)
+    @PostMapping("/clothing")
+    public String addClothing(@PathVariable String color) {
+        return "Hello, " + name + "!";
+    }
+
+    //PUT: updating attributes of the clothing piece (id)
+    @GPutMapping("/clothing/{id}")
+    public String updateClothing(@PathVariable String id) {
+        return "Hello, " + name + "!";
+    }
+
+    //DELETE: removing clothing piece from db (id)
+    @DeleteMapping("/clothing/{id}")
+    public String removeClothing(@PathVariable String id) {
+        return "Hello, " + name + "!";
+    }
+
+    // GET: getting clothing pieces from the db (id)
+    @GetMapping("/clothing/{id}")
+    public String getClothing(@PathVariable String id) {
+        return "Hello, " + name + "!";
+    }
+    // Add more methods for other API endpoints as needed
+}
