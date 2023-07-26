9a67d6db73081860a9d4a06b5b5ac1cd8ba69a3d
Himel Uddin <hmu4127@rit.edu>
2023-07-26 09:14:38 -0400

    Fixed FitCheckAPIController controller issues

--- FitCheck_spring/src/main/java/com/example/FitCheck/api/FitCheckAPIController.java
+++ FitCheck_spring/src/main/java/com/example/FitCheck/api/FitCheckAPIController.java
@@ -1,11 +1,10 @@
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+package com.example.FitCheck.api;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/") // Base URL mapping for all endpoints in this controller
-public class MyController {
+public class FitCheckAPIController {
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -15,25 +14,25 @@ public class MyController {
     // POST: initially adding clothing piece to the db (clothing attributes...)
     @PostMapping("/clothing")
     public String addClothing(@PathVariable String color) {
-        return "Hello, " + name + "!";
+        return "Hello, " + color + "!";
     }
 
     //PUT: updating attributes of the clothing piece (id)
-    @GPutMapping("/clothing/{id}")
+    @PutMapping("/clothing/{id}")
     public String updateClothing(@PathVariable String id) {
-        return "Hello, " + name + "!";
+        return "Hello, " + id + "!";
     }
 
     //DELETE: removing clothing piece from db (id)
     @DeleteMapping("/clothing/{id}")
     public String removeClothing(@PathVariable String id) {
-        return "Hello, " + name + "!";
+        return "Hello, " + id + "!";
     }
 
     // GET: getting clothing pieces from the db (id)
     @GetMapping("/clothing/{id}")
     public String getClothing(@PathVariable String id) {
-        return "Hello, " + name + "!";
+        return "Hello, " + id + "!";
     }
     // Add more methods for other API endpoints as needed
 }
