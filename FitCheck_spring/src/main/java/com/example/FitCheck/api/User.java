d775a3c8748847581215353991cfb303678c8aa9
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-08-02 17:22:17 -0400

    User Model

---
+++ FitCheck_spring/src/main/java/com/example/FitCheck/api/User.java
@@ -0,0 +1,15 @@
+package com.example.FitCheck.api;
+
+public class User {
+    String username;
+    String password;
+    String name;
+    String email;
+
+    public User(String username, String password, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
