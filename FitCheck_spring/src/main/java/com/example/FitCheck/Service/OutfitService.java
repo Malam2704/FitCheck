91a6b06f46226b4203453fa58de639c138c8094d
Borna5356 <bornakevin1@gmail.com>
2023-08-07 16:55:14 -0400

    Created outline for Outfit server class

--- FitCheck_spring/src/main/java/com/example/FitCheck/Service/OutfitService.java
+++ FitCheck_spring/src/main/java/com/example/FitCheck/Service/OutfitService.java
@@ -1,10 +1,32 @@
 package com.example.FitCheck.Service;
 
 import com.example.FitCheck.Model.Outfit;
+import com.example.FitCheck.repository.OutfitRepo;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Service;
 
 
 public class OutfitService {
-    
+    private OutfitRepo repo;
+
+    public String getId() {
+        return "";
+    }
+
+    public void createId(String id) {
+        return;
+    }
+
+    public void createOutfit(Outfit outfit) {
+        return;
+    }
+
+    public void updateOutfit(Outfit outfit) {
+        return;
+    }
+
+    public void deleteOutfit(Outfit outfit) {
+        return;
+    }
 }
---
+++ FitCheck_spring/src/main/java/com/example/FitCheck/repository/OutfitRepo.java
@@ -0,0 +1,8 @@
+package com.example.FitCheck.repository;
+
+import com.example.FitCheck.Model.Outfit;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface OutfitRepo {
+    
+}
